1fc9a80bff80183e580aeb5f6f19b4320244511a
Meint-Willem Gaasbeek <gaasbeekmw@gmail.com>
2017-09-25 17:30:25 +0200

    basis acl excecution added

--- app/controllers/ControllerBase.php
+++ app/controllers/ControllerBase.php
@@ -116,51 +116,16 @@ class ControllerBase extends Controller
         $controllerName = $dispatcher->getControllerName();
         $actionName     = $dispatcher->getActionName();
 
-        if (!$this->acl->isAllowed('guest', $controllerName, $actionName)) {
+        if (!$this->acl->isAllowed('user', $controllerName, $actionName)) {
             $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
-            //exit;
+
+            $dispatcher->forward([
+                'controller' => 'index',
+                'action'     => 'index'
+            ]);
+            return FALSE;
         }
 
-        // Only check permissions on private controllers
-        // if ($this->acl->isPrivate($controllerName)) {
-        //
-        //     // Get the current identity
-        //     $identity = $this->auth->getIdentity();
-        //
-        //     // If there is no identity available the user is redirected to index/index
-        //     if (!is_array($identity)) {
-        //
-        //         $this->flash->notice('You don\'t have access to this module: private');
-        //
-        //         $dispatcher->forward([
-        //             'controller' => 'index',
-        //             'action' => 'index'
-        //         ]);
-        //         return false;
-        //     }
-        //
-        //     // Check if the user have permission to the current option
-        //     $actionName = $dispatcher->getActionName();
-        //
-        //     if (!$this->acl->isAllowed($identity['profile'], $controllerName, $actionName)) {
-        //
-        //         $this->flash->notice('You don\'t have access to this module: ' . $controllerName . ':' . $actionName);
-        //
-        //         if ($this->acl->isAllowed($identity['profile'], $controllerName, 'index')) {
-        //             $dispatcher->forward([
-        //                 'controller' => $controllerName,
-        //                 'action' => 'index'
-        //             ]);
-        //         } else {
-        //             $dispatcher->forward([
-        //                 'controller' => 'user_control',
-        //                 'action' => 'index'
-        //             ]);
-        //         }
-        //
-        //         return false;
-        //     }
-        // }
     }
 
 }
