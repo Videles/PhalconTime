4d764692bbe9695a33fef912cd600af068cd1ec1
Meint-Willem Gaasbeek <gaasbeekmw@gmail.com>
2017-09-26 16:48:09 +0200

    bugfix filter integer

--- app/forms/ProjectForm.php
+++ app/forms/ProjectForm.php
@@ -255,7 +255,7 @@ class ProjectForm extends Form
         $delivered->setFilters(
             [
                 "striptags",
-                "integer",
+                "int",
             ]
         );
         $this->add($delivered);
